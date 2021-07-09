501b2b97fb8c5f3d4abf6f0654558c5faa8a1a52
HenricoW <henrico.ashley.White@gmail.com>
2021-07-09 16:44:04 +0200

    ported express app to firebase project

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,32 @@
 const functions = require("firebase-functions");
+const express = require("express");
+const mongoose = require("mongoose");
+require("dotenv/config");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const productRoutes = require("../routes/productRoutes");
+
+const app = express();
+
+mongoose.connect(process.env.DB_CONN_STRING, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log("connected to mongo!");
+});
+
+app.get("/", (req, res) => {
+    res.status(200).send("Hello from firebase functions!");
+});
+
+app.use("/products", productRoutes);
+
+// catch-all for unsupported end points
+app.use((req, res, next) => {
+    const error = new Error("route not found");
+    error.status = 404;
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message,
+        },
+    });
+});
+
+exports.api = functions.https.onRequest(app);
