69b27323b876eea56234a7239490769af7c187ad
HenricoW <henrico.ashley.White@gmail.com>
2021-08-02 17:43:41 +0200

    integrated mongodb

--- functions/index.js
+++ functions/index.js
@@ -1,24 +1,31 @@
 const functions = require("firebase-functions");
 const express = require("express");
 const mongoose = require("mongoose");
-require("dotenv/config");
+const cors = require("cors");
+// require("dotenv/config");
 
-const productRoutes = require("../routes/productRoutes");
+const productRoutes = require("./routes/productRoutes");
 
-const app = express();
+// const CONN_STRING = functions.config().db ? functions.config().db.conn_string : process.env.DB_CONN_STRING;
+const CONN_STRING = functions.config().db.conn_string;
 
-mongoose.connect(process.env.DB_CONN_STRING, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+mongoose.connect(CONN_STRING, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+    if (err) console.log(err);
     console.log("connected to mongo!");
 });
 
-app.get("/", (req, res) => {
+const mongo_app = express();
+
+mongo_app.use(cors({ origin: true }));
+
+mongo_app.get("/", (req, res) => {
     res.status(200).send("Hello from firebase functions!");
 });
 
-app.use("/products", productRoutes);
+mongo_app.use("/products", productRoutes);
 
 // catch-all for unsupported end points
-app.use((req, res, next) => {
+mongo_app.use((req, res) => {
     const error = new Error("route not found");
     error.status = 404;
     res.status(error.status || 500);
@@ -29,4 +36,4 @@ app.use((req, res, next) => {
     });
 });
 
-exports.api = functions.https.onRequest(app);
+exports.api = functions.https.onRequest(mongo_app);
