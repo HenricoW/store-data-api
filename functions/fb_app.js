056d35f213e0ae1fb233be8df6b61660d860bcf2
HenricoW <henrico.ashley.White@gmail.com>
2021-08-06 22:18:50 +0200

    added review routes

--- functions/fb_app.js
+++ functions/fb_app.js
@@ -5,13 +5,11 @@ const cors = require("cors");
 
 const productRoutes = require("./routes/fb_productRoutes");
 const nftRoutes = require("./routes/fb_nftRoutes");
+const reviewRoutes = require("./routes/fb_reviewRoutes");
 
 admin.initializeApp();
-
 const fb_app = express();
-
 fb_app.use(cors({ origin: true }));
-
 console.log("In FB app!");
 
 fb_app.get("/", (req, res) => {
@@ -20,5 +18,6 @@ fb_app.get("/", (req, res) => {
 
 fb_app.use("/products", productRoutes);
 fb_app.use("/nft", nftRoutes);
+fb_app.use("/reviews", reviewRoutes);
 
 module.exports = fb_app;
---
+++ functions/routes/fb_reviewRoutes.js
@@ -0,0 +1,159 @@
+const express = require("express");
+const firebase = require("firebase");
+
+const db = firebase.firestore();
+const review_db = db.collection("reviews");
+
+const router = express.Router();
+
+// const pathFiller = "/store-w3-api/us-central1/api2"; // in dev
+const pathFiller = "/api2"; // in prod
+
+const validateData = (req, res, next) => {
+    const validFields = ["name", "imageUrl", "text"];
+    const reqFields = Object.keys(req.body);
+    let hasError = false;
+
+    // check if all fields are present
+    for (let i = 0; i < validFields.length; i++) {
+        if (!reqFields.includes(validFields[i])) {
+            const message = `Field: '${validFields[i]}' not present in request body`;
+            hasError = true;
+            res.status(406).json({
+                error: {
+                    message,
+                },
+            });
+            break;
+        }
+    }
+
+    if (!hasError) {
+        console.log("Request data validated*");
+        next();
+    }
+};
+
+router.get("/", (req, res) => {
+    let reviews = [];
+    const basePath = req.protocol + "://" + req.get("host") + pathFiller + req.baseUrl;
+    console.log("in /review");
+    review_db
+        // .orderBy("createdAt")
+        // .limit(1)
+        .get()
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                error: {
+                    message: "Error retrieving data from database",
+                },
+            });
+        })
+        .then((snap) => {
+            console.log("in snapshot block");
+            snap.forEach((doc) => {
+                let item = doc.data();
+                const obj = {
+                    id: doc.id,
+                    ...item,
+                    path: basePath + "/" + doc.id,
+                };
+                reviews.push(obj);
+            });
+
+            res.status(200).json(reviews);
+        });
+});
+
+router.get("/:review_id", (req, res) => {
+    const review_id = req.params.review_id;
+    const parent = req.protocol + "://" + req.get("host") + pathFiller + req.baseUrl;
+    const fullUrl = parent + req.path;
+    console.log(review_id);
+    review_db
+        .doc(review_id)
+        .get()
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                error: {
+                    message: "Error retrieving data from database",
+                },
+            });
+        })
+        .then((doc) => {
+            item = doc.data();
+            if (!item)
+                res.status(404).json({
+                    error: {
+                        message: "Requested resource not found",
+                    },
+                });
+            else
+                res.status(200).json({
+                    id: doc.id,
+                    ...item,
+                    paths: {
+                        this: fullUrl,
+                        parent,
+                    },
+                });
+        });
+});
+
+router.post("/", validateData, (req, res) => {
+    if (res.error) {
+        // res.send();
+        return;
+    }
+    const addedOn = firebase.firestore.Timestamp.fromDate(new Date());
+    const postData = {
+        name: req.body.name,
+        imageUrl: req.body.imageUrl,
+        text: req.body.text,
+        addedOn,
+    };
+    review_db
+        .add(postData)
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                error: {
+                    message: "Error adding data to database",
+                },
+            });
+        })
+        .then((docRef) => {
+            console.log(`Review was written to firestore with id: ${docRef.id}`);
+            res.status(201).json({
+                id: docRef.id,
+                ...postData,
+            });
+        });
+});
+
+router.delete("/:review_id", (req, res) => {
+    const review_id = req.params.review_id;
+    review_db
+        .doc(review_id)
+        .delete()
+        .catch((err) => {
+            console.log(err);
+            res.status(500).json({
+                error: {
+                    message: "Error deleting the document",
+                },
+            });
+        })
+        .then(() => {
+            console.log(`review id: ${review_id} successfully deleted`);
+            res.status(200).json({
+                success: {
+                    message: "Document successfully deleted",
+                },
+            });
+        });
+});
+
+module.exports = router;
