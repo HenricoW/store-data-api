8a9f505653d2fa75b0473653a3b4615568b3fd83
HenricoW <henrico.ashley.White@gmail.com>
2022-02-05 17:19:25 +0200

    nomenclature

--- functions/middleware/inputValidation.js
+++ functions/middleware/inputValidation.js
@@ -7,173 +7,173 @@ const nft_db = db.collection("nft");
 
 // check if all required fields are present in the request body
 const allFieldsPresent = (req, requiredFields) => {
-    const requestFields = Object.keys(req.body);
+  const requestFields = Object.keys(req.body);
 
-    for (let i = 0; i < requiredFields.length; i++) {
-        if (!requestFields.includes(requiredFields[i])) {
-            const message = `Field: '${requiredFields[i]}' not present in request body`;
-            return { error: true, message };
-        }
+  for (let i = 0; i < requiredFields.length; i++) {
+    if (!requestFields.includes(requiredFields[i])) {
+      const message = `Field: '${requiredFields[i]}' not present in request body`;
+      return { error: true, message };
     }
+  }
 
-    return { error: false, message: null };
+  return { error: false, message: null };
 };
 
 exports.productValidation = (req, res, next) => {
-    const validFields = ["title", "desc", "price", "imageUrl", "message", "signedMssg"];
-    let hasError = false;
+  const validFields = ["title", "desc", "price", "imageUrl", "message", "signedMssg"];
+  let hasError = false;
 
-    // check if all fields are present
-    const allData = allFieldsPresent(req, validFields);
-    if (allData.error) {
-        res.status(406).json({
-            error: {
-                message: allData.message,
-            },
-        });
-        return;
-    }
+  // check if all fields are present
+  const allData = allFieldsPresent(req, validFields);
+  if (allData.error) {
+    res.status(406).json({
+      error: {
+        message: allData.message,
+      },
+    });
+    return;
+  }
 
-    // get signer address
-    let address;
-    try {
-        address = getSignerAddress(req.body.message, req.body.signedMssg);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            error: {
-                message: "Error extracting signer address",
-                detail: err.message,
-            },
-        });
-        return;
-    }
-    console.log(address);
+  // get signer address
+  let address;
+  try {
+    address = getSignerAddress(req.body.message, req.body.signedMssg);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: {
+        message: "Error extracting signer address",
+        detail: err.message,
+      },
+    });
+    return;
+  }
+  console.log(address);
 
-    // ...further validation/sanitization...
+  // ...further validation/sanitization...
 
-    if (!hasError) {
-        console.log("Request data validated*");
-        next();
-    }
+  if (!hasError) {
+    console.log("Request data validated*");
+    next();
+  }
 };
 
 exports.productDeletion = (req, res, next) => {
-    const validFields = ["message", "signedMssg"];
-    let hasError = false;
+  const validFields = ["message", "signedMssg"];
+  let hasError = false;
 
-    // check if all fields are present
-    const allData = allFieldsPresent(req, validFields);
-    if (allData.error) {
-        res.status(406).json({
-            error: {
-                message: allData.message,
-            },
-        });
-        return;
-    }
+  // check if all fields are present
+  const allData = allFieldsPresent(req, validFields);
+  if (allData.error) {
+    res.status(406).json({
+      error: {
+        message: allData.message,
+      },
+    });
+    return;
+  }
 
-    // get signer address
-    let address;
-    try {
-        address = getSignerAddress(req.body.message, req.body.signedMssg);
-    } catch (err) {
-        console.log(err);
-        res.status(500).json({
-            error: {
-                message: "Error extracting signer address",
-                detail: err.message,
-            },
-        });
-        return;
-    }
-    console.log(address);
+  // get signer address
+  let address;
+  try {
+    address = getSignerAddress(req.body.message, req.body.signedMssg);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      error: {
+        message: "Error extracting signer address",
+        detail: err.message,
+      },
+    });
+    return;
+  }
+  console.log(address);
 
-    // ...further validation/sanitization...
+  // ...further validation/sanitization...
 
-    if (!hasError) {
-        console.log("Request data validated*");
-        next();
-    }
+  if (!hasError) {
+    console.log("Request data validated*");
+    next();
+  }
 };
 
 exports.nftValidation = (req, res, next) => {
-    const validFields = ["title", "desc", "imageUrl", "itemID", "tokenID"];
-    let hasError = false;
+  const validFields = ["name", "description", "image", "itemID", "tokenID"];
+  let hasError = false;
 
-    // check if all fields are present
-    const allData = allFieldsPresent(req, validFields);
-    if (allData.error) {
-        res.status(406).json({
-            error: {
-                message: allData.message,
-            },
+  // check if all fields are present
+  const allData = allFieldsPresent(req, validFields);
+  if (allData.error) {
+    res.status(406).json({
+      error: {
+        message: allData.message,
+      },
+    });
+    return;
+  }
+
+  // check if tokenID is already used
+  if (!hasError)
+    nft_db
+      .where("tokenID", "==", parseInt(req.body.tokenID))
+      .get()
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json({
+          error: {
+            message: "Error verifying tokenID",
+          },
         });
-        return;
-    }
+      })
+      .then((snap) => {
+        if (!snap) {
+          res.status(404).json({
+            error: {
+              message: "Empty resource returned",
+            },
+          });
+        } else {
+          let itemArray = [];
+          // snapshot obj: can't just do snap.length, have to 'unwrap' with forEach()
+          snap.forEach((doc) => {
+            itemArray.push(doc.data());
+          });
 
-    // check if tokenID is already used
-    if (!hasError)
-        nft_db
-            .where("tokenID", "==", parseInt(req.body.tokenID))
-            .get()
-            .catch((err) => {
-                console.log(err);
-                res.status(500).json({
-                    error: {
-                        message: "Error verifying tokenID",
-                    },
-                });
-            })
-            .then((snap) => {
-                if (!snap) {
-                    res.status(404).json({
-                        error: {
-                            message: "Empty resource returned",
-                        },
-                    });
-                } else {
-                    let itemArray = [];
-                    // snapshot obj: can't just do snap.length, have to 'unwrap' with forEach()
-                    snap.forEach((doc) => {
-                        itemArray.push(doc.data());
-                    });
-
-                    if (itemArray.length > 0) {
-                        res.status(400).json({
-                            error: {
-                                message: "tokenID already taken",
-                            },
-                        });
-                    } else {
-                        // ...further validation/sanitization...
-
-                        console.log("Request data validated*");
-                        next();
-                    }
-                }
+          if (itemArray.length > 0) {
+            res.status(400).json({
+              error: {
+                message: "tokenID already taken",
+              },
             });
+          } else {
+            // ...further validation/sanitization...
+
+            console.log("Request data validated*");
+            next();
+          }
+        }
+      });
 };
 
 exports.reviewValidation = (req, res, next) => {
-    const validFields = ["name", "imageUrl", "text"];
-    let hasError = false;
+  const validFields = ["name", "imageUrl", "text"];
+  let hasError = false;
 
-    // check if all fields are present
-    const allData = allFieldsPresent(req, validFields);
-    if (allData.error) {
-        res.status(406).json({
-            error: {
-                message: allData.message,
-            },
-        });
-        return;
-    }
+  // check if all fields are present
+  const allData = allFieldsPresent(req, validFields);
+  if (allData.error) {
+    res.status(406).json({
+      error: {
+        message: allData.message,
+      },
+    });
+    return;
+  }
 
-    // ...further validation/sanitization...
+  // ...further validation/sanitization...
 
-    if (!hasError) {
-        console.log("Request data validated*");
-        next();
-    }
+  if (!hasError) {
+    console.log("Request data validated*");
+    next();
+  }
 };
--- functions/routes/fb_nftRoutes.js
+++ functions/routes/fb_nftRoutes.js
@@ -15,128 +15,128 @@ router.use(fbAuthorize);
 // ROUTES
 // get all nfts
 router.get("/", (req, res) => {
-    let products = [];
-    const basePath = req.protocol + "://" + req.get("host") + pathFiller + req.baseUrl;
-    console.log("in /nft");
-    nft_db
-        // .orderBy("createdAt")
-        // .limit(1)
-        .get()
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json({
-                error: {
-                    message: "Error retrieving data from database",
-                },
-            });
-        })
-        .then((snap) => {
-            if (!snap) {
-                res.status(404).json({
-                    error: {
-                        message: "Empty resource returned",
-                    },
-                });
-            } else {
-                snap.forEach((doc) => {
-                    let item = doc.data();
-                    const obj = {
-                        id: doc.id,
-                        ...item,
-                        path: basePath + "/" + item.tokenID,
-                    };
-                    products.push(obj);
-                });
-
-                res.status(200).json(products);
-            }
+  let products = [];
+  const basePath = req.protocol + "://" + req.get("host") + pathFiller + req.baseUrl;
+  console.log("in /nft");
+  nft_db
+    // .orderBy("createdAt")
+    // .limit(1)
+    .get()
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: {
+          message: "Error retrieving data from database",
+        },
+      });
+    })
+    .then((snap) => {
+      if (!snap) {
+        res.status(404).json({
+          error: {
+            message: "Empty resource returned",
+          },
         });
+      } else {
+        snap.forEach((doc) => {
+          let item = doc.data();
+          const obj = {
+            id: doc.id,
+            ...item,
+            path: basePath + "/" + item.tokenID,
+          };
+          products.push(obj);
+        });
+
+        res.status(200).json(products);
+      }
+    });
 });
 
 // get specific nft by contract generated token id
 router.get("/:token_id", (req, res) => {
-    const token_id = req.params.token_id;
-    const parent = req.protocol + "://" + req.get("host") + pathFiller + req.baseUrl;
-    const fullUrl = parent + req.path;
-    console.log(token_id);
-    nft_db
-        .where("tokenID", "==", parseInt(token_id))
-        .get()
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json({
-                error: {
-                    message: "Error retrieving data from database",
-                },
-            });
-        })
-        .then((snap) => {
-            if (!snap) {
-                res.status(404).json({
-                    error: {
-                        message: "Empty resource returned",
-                    },
-                });
-            } else {
-                let itemArray = [];
-                snap.forEach((doc) => {
-                    itemArray.push(doc.data());
-                });
-                const data = itemArray[0];
-                if (itemArray.length === 0)
-                    res.status(404).json({
-                        error: {
-                            message: "Requested resource not found",
-                        },
-                    });
-                else
-                    res.status(200).json({
-                        title: data.title,
-                        desc: data.desc,
-                        imageUrl: data.imageUrl,
-                        itemID: data.itemID,
-                        tokenID: data.tokenID,
-                        purchasedOn: data.purchasedOn,
-                        paths: {
-                            this: fullUrl,
-                            parent,
-                        },
-                    });
-            }
+  const token_id = req.params.token_id;
+  const parent = req.protocol + "://" + req.get("host") + pathFiller + req.baseUrl;
+  const fullUrl = parent + req.path;
+  console.log(token_id);
+  nft_db
+    .where("tokenID", "==", parseInt(token_id))
+    .get()
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: {
+          message: "Error retrieving data from database",
+        },
+      });
+    })
+    .then((snap) => {
+      if (!snap) {
+        res.status(404).json({
+          error: {
+            message: "Empty resource returned",
+          },
         });
+      } else {
+        let itemArray = [];
+        snap.forEach((doc) => {
+          itemArray.push(doc.data());
+        });
+        const data = itemArray[0];
+        if (itemArray.length === 0)
+          res.status(404).json({
+            error: {
+              message: "Requested resource not found",
+            },
+          });
+        else
+          res.status(200).json({
+            name: data.name,
+            description: data.description,
+            image: data.image,
+            itemID: data.itemID,
+            tokenID: data.tokenID,
+            purchasedOn: data.purchasedOn,
+            paths: {
+              this: fullUrl,
+              parent,
+            },
+          });
+      }
+    });
 });
 
 // add a new nft to 'nfts' collection. return item data + firestore id on success
 router.post("/", nftValidation, (req, res) => {
-    const purchasedOn = timestamp_fb();
+  const purchasedOn = timestamp_fb();
 
-    // extract only the needed information from the request
-    const postData = {
-        title: req.body.title,
-        desc: req.body.desc,
-        imageUrl: req.body.imageUrl,
-        itemID: req.body.itemID,
-        tokenID: parseInt(req.body.tokenID),
-        purchasedOn,
-    };
+  // extract only the needed information from the request
+  const postData = {
+    name: req.body.name,
+    description: req.body.description,
+    image: req.body.image,
+    itemID: req.body.itemID,
+    tokenID: parseInt(req.body.tokenID),
+    purchasedOn,
+  };
 
-    nft_db
-        .add(postData)
-        .catch((err) => {
-            console.log(err);
-            res.status(500).json({
-                error: {
-                    message: "Error adding data to database",
-                },
-            });
-        })
-        .then((docRef) => {
-            console.log(`Document was written to firestore with id: ${docRef.id}`);
-            res.status(201).json({
-                id: docRef.id,
-                ...postData,
-            });
-        });
+  nft_db
+    .add(postData)
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json({
+        error: {
+          message: "Error adding data to database",
+        },
+      });
+    })
+    .then((docRef) => {
+      console.log(`Document was written to firestore with id: ${docRef.id}`);
+      res.status(201).json({
+        id: docRef.id,
+        ...postData,
+      });
+    });
 });
 
 // delete nft entry from 'nfts' collection
